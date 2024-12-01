4059e4fcb10bbf4aa1d972bdbfdba76b47252f28
Lukas Demetz <lukas.demetz@fh-kufstein.ac.at>
2024-12-01 06:53:13 +0100

    Solution day 1

--- src/1/index.ts
+++ src/1/index.ts
@@ -1,8 +1,29 @@
+import { sum } from '../utils/math';
 import { readLinesFromInput, getInputFileName } from '../utils/readFile';
+import { zip } from 'lodash';
 
 export async function main() {
-  const input = await readLinesFromInput(getInputFileName(__dirname));
+  const input = (await readLinesFromInput(getInputFileName(__dirname))).map(
+    (line) => line.split(/ +/)
+  );
 
-  console.log('Part 1:');
-  console.log('Part 2:');
+  const left = input.flatMap(([a]) => Number(a));
+  const right = input.flatMap(([_, b]) => Number(b));
+
+  left.sort();
+  right.sort();
+
+  console.log(
+    'Part 1:',
+    zip(left, right)
+      .map(([a, b]) => Math.abs(a - b))
+      .reduce(sum)
+  );
+
+  console.log(
+    'Part 2:',
+    left
+      .map((value) => right.filter((other) => other === value).length * value)
+      .reduce(sum)
+  );
 }
